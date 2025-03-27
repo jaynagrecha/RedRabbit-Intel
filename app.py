c2952ef9a9e8b7f5176a9e41dec740dc9924436e
Jay Nagrecha <jaynagrecha98@gmail.com>
2025-03-27 18:38:48 +0530

    Fix SQLAlchemy import issue and remove database setup

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from routes.root import root_bp
 
-db = SQLAlchemy()
+#db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
     
-    db.init_app(app)
+   # db.init_app(app)
     jwt.init_app(app)
 
     from routes.ioc import ioc_bp
