d9d45514542ccaace70219752697a129130817bf
Jay Nagrecha <jaynagrecha98@gmail.com>
2025-03-27 19:29:22 +0530

    Installed Cors and change app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS  # Import the CORS module
 from routes.root import root_bp
 
 db = SQLAlchemy()
@@ -12,6 +13,9 @@ def create_app():
     
     db.init_app(app)
     jwt.init_app(app)
+
+    # Enable CORS for all routes (optional: configure it more precisely if needed)
+    CORS(app)
 
     from routes.ioc import ioc_bp
     app.register_blueprint(root_bp)
